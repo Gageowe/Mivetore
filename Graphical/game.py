b6f6b611ccd1518cf75f3eb0c318460f10757dcc
Gage <gageowe@gmail.com>
2019-01-24 13:09:14 +0800

    Creating branch for quick collision detection and stuff

--- Graphical/game.py
+++ Graphical/game.py
@@ -14,27 +14,31 @@ def main():
 	image01 = pygame.image.load("01_image.png")
 	
 	image01.set_colorkey((255,255,255))
-	screen.blit(image01, (50,50))
+	#screen.blit(image01, (50,50))
 	pygame.display.flip()
 	xpos = 50
 	ypos = 50
 	step_x = 10
 	step_y = 10
 	
+	_update = pygame.sprite.RenderUpdates()
+	_update.add(image01)
 	
 	while running:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = False
-		screen.blit(background, (0,0))
-		screen.blit(image01, (xpos,ypos))
-		pygame.display.flip()
+		#screen.blit(background, (0,0))
+		#screen.blit(image01, (xpos,ypos))
+		
+		#pygame.display.flip()
 		if xpos >208 or xpos < 0:
 			step_x = -step_x
 		if ypos >148 or ypos<0 :
 			step_y = -step_y
 		xpos +=step_x
 		ypos +=step_y
+		_update.draw()
 		
 		
 if __name__ == "__main__":
