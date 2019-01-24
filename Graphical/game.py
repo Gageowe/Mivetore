8f03779b9cd3bbf3ce2ecf438ce91124633b193a
Gage <gageowe@gmail.com>
2019-01-24 13:27:35 +0800

    Unsuccessfully increased game capabilities , added some 'art'

--- Graphical/game.py
+++ Graphical/game.py
@@ -1,15 +1,15 @@
-import pygame
+import pygame,time
 def main():
 	pygame.init()
 	logo = pygame.image.load("logo32x32.png")
 	pygame.display.set_icon(logo)
 	pygame.display.set_caption("minimal program")
 	
-	screen = pygame.display.set_mode((240,180))
+	screen = pygame.display.set_mode((960,270))
 	
 	running = True
 	
-	background = pygame.image.load("background.png")
+	background = pygame.image.load("background02.png")
 	screen.blit(background, (0,0))
 	image01 = pygame.image.load("01_image.png")
 	
@@ -24,21 +24,27 @@ def main():
 	_update = pygame.sprite.RenderUpdates()
 	_update.add(image01)
 	
+	lastTime=time.process_time()
+	newTime=time.process_time()
 	while running:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				running = False
+				newTime=time.process_time()
+				since = newTime - lastTime
+				if since >= 30 :
+						for event in pygame.event.get():
+								if event.type == pygame.QUIT:
+										running = False
 		#screen.blit(background, (0,0))
 		#screen.blit(image01, (xpos,ypos))
 		
 		#pygame.display.flip()
-		if xpos >208 or xpos < 0:
-			step_x = -step_x
-		if ypos >148 or ypos<0 :
-			step_y = -step_y
-		xpos +=step_x
-		ypos +=step_y
-		_update.draw()
+						if xpos >928 or xpos < 0:
+								step_x = -step_x
+						if ypos >688 or ypos<0 :
+								step_y = -step_y
+						xpos +=step_x
+						ypos +=step_y
+						_update.draw()
+						lastTime = newTime
 		
 		
 if __name__ == "__main__":
